3462f4008af247a6441de03b2d5a329142b9b8cb
Angus Muller <angusmuller@anguss-mbp.lan>
2017-11-13 11:59:11 +0000

    Refactored for master table datasource to be in its own class

--- Diary App/Controller/DiaryMasterController.swift
+++ Diary App/Controller/DiaryMasterController.swift
@@ -13,105 +13,15 @@ class DiaryMasterController: UITableViewController {
     
     let managedObjectContext = CoreDataStack().managedObjectContext
     
-    // Use fetched results controller
-    lazy var fetchedResultsController: DiaryFetchedResultsController = {
-        return DiaryFetchedResultsController(managedObjectContext: self.managedObjectContext, tableView: self.tableView)
+    lazy var dataSource: DiaryMasterDataSource = {
+        return DiaryMasterDataSource(tableView: self.tableView, context: managedObjectContext)
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-   
+        tableView.dataSource = dataSource
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        
-        return fetchedResultsController.sections?.count ?? 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        guard let section = fetchedResultsController.sections?[section] else { return 0 }
-        return section.numberOfObjects
-    }
-
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "EntryCell", for: indexPath)
-
-        return configureCell(cell, at: indexPath)
-    }
-    
-    // Set action for delete button
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        let entry = fetchedResultsController.object(at: indexPath)
-        managedObjectContext.delete(entry)
-        managedObjectContext.saveChanges()
-    }
-    
-    // Helper to return configured cell
-    private func configureCell(_ cell: UITableViewCell, at indexPath: IndexPath) -> UITableViewCell {
-        let entry = fetchedResultsController.object(at: indexPath)
-        
-        cell.textLabel?.text = entry.text
-        cell.detailTextLabel?.text = entry.dateCreated?.description
-        
-        return cell
-    }
-
-    
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
     
     // Mark: UITableViewDelegate
     
@@ -129,7 +39,7 @@ class DiaryMasterController: UITableViewController {
         } else if segue.identifier == "showDetail" {
             guard let detailsViewController = segue.destination as? ViewEditEntryController, let indexPath = tableView.indexPathForSelectedRow else { return }
             
-            let entry = fetchedResultsController.object(at: indexPath)
+            let entry = dataSource.object(at: indexPath)
             detailsViewController.entry = entry // pass over selected entry data
             detailsViewController.context = self.managedObjectContext // send referance of context
         }
---
+++ Diary App/DataSource/DiaryMasterDataSource.swift
@@ -0,0 +1,75 @@
+//
+//  DiaryMasterDataSource.swift
+//  Diary App
+//
+//  Created by Angus Muller on 13/11/2017.
+//  Copyright © 2017 Angus Muller. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class DiaryMasterDataSource: NSObject, UITableViewDataSource {
+    private let tableView: UITableView
+    private let managedObjectContext: NSManagedObjectContext
+    
+    // Use fetched results controller
+    lazy var fetchedResultsController: DiaryFetchedResultsController = {
+        return DiaryFetchedResultsController(managedObjectContext: self.managedObjectContext, tableView: self.tableView)
+    }()
+    
+    init(tableView: UITableView, context: NSManagedObjectContext) {
+        self.tableView = tableView
+        self.managedObjectContext = context
+    }
+    
+    // MARK: - Table view data source
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return fetchedResultsController.sections?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        guard let section = fetchedResultsController.sections?[section] else { return 0 }
+        return section.numberOfObjects
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "EntryCell", for: indexPath)
+        
+        return configureCell(cell, at: indexPath)
+    }
+    
+    // Set action for delete button
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        let entry = fetchedResultsController.object(at: indexPath)
+        managedObjectContext.delete(entry)
+        managedObjectContext.saveChanges()
+    }
+    
+    // Helper to return configured cell
+    private func configureCell(_ cell: UITableViewCell, at indexPath: IndexPath) -> UITableViewCell {
+        let entry = fetchedResultsController.object(at: indexPath)
+        
+        cell.textLabel?.text = entry.text
+        cell.detailTextLabel?.text = entry.dateCreated?.description
+        
+        return cell
+    }
+    
+    func object(at indexPath: IndexPath) -> Entry {
+        return fetchedResultsController.object(at: indexPath)
+    }
+    
+    
+    
+
+    
+
+    
+
+ 
+}
