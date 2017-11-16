d3c807c009b5f2286d101a7a2f2ec94054993a95
Angus Muller <angusmuller@anguss-mbp.lan>
2017-11-16 11:41:51 +0000

    Getting coordinates back, need to be text location instead, location authorisation popup needs fixing

--- Diary App/Controller/CurrentLocationController.swift
+++ Diary App/Controller/CurrentLocationController.swift
@@ -7,13 +7,14 @@
 //
 
 import UIKit
+import CoreLocation
 
 class CurrentLocationController: UIViewController {
     
     // MARK: Properties
     
     lazy var locationManager: LocationManager = {
-        return LocationManager(delegate: self, permissionsDelegate: nil)
+        return LocationManager(delegate: self, permissionsDelegate: self)
     }()
     
     var coordinate: Coordinate?
@@ -36,12 +37,21 @@ class CurrentLocationController: UIViewController {
         } else {
             //Do Permissions
             print("do permissions")
+            requestLocationPermissions()
         }
     }
 
     // MARK: Permissions
     //To setup permissions func
-    
+    func requestLocationPermissions() {
+        do {
+            try locationManager.requestLocationAuthorization()
+        } catch LocationError.disallowedByUser {
+            // Show alert to user
+        } catch let error {
+            print("Location Authorization Error: \(error.localizedDescription)")
+        }
+    }
 
 }
 
@@ -49,10 +59,50 @@ class CurrentLocationController: UIViewController {
 extension CurrentLocationController: LocationManagerDelegate {
     func obtainedCoordinates(_ coordinate: Coordinate) {
         self.coordinate = coordinate
+        print(coordinate)
        
     }
     
     func failedWithError(_ error: LocationError) {
         print(error)
+    }
+}
+
+//Mark: Location Permissions Delegate
+extension CurrentLocationController: LocationPermissionsDelegate {
+    
+    
+    func authorizationSucceeded() {
+        // NOTE: I Do not know if i need t oenter anything in here
+        print("hit auhtorizationsucceeded func")
+        locationManager.requestLocation()
+    }
+ 
+    
+    func authorizationFailedWithStatus(_ status: CLAuthorizationStatus) {
+        
+        let alertController = UIAlertController(title: "Need permission", message: "Message", preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        
+        let settingsAction = UIAlertAction(title: "Settings", style: .default) {
+            UIAlertAction in
+            if let url = URL(string: UIApplicationOpenSettingsURLString) {
+                if #available(iOS 10, *) {
+                    UIApplication.shared.open(url, options: [:],
+                                              completionHandler: { (success) in
+                                                print("Open \(UIApplicationOpenSettingsURLString): \(success)")
+                    })
+                } else {
+                    let success = UIApplication.shared.openURL(url)
+                    print("Open \(UIApplicationOpenSettingsURLString): \(success)")
+                }
+            }
+        }
+        
+        alertController.addAction(okAction)
+        alertController.addAction(settingsAction)
+        
+        present(alertController, animated: true, completion: nil)
     }
 }
